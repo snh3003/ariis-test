e7538b4e58b9a989704208dcb9eca9ad47325231
snh3003 <shahsama542@gmail.com>
2021-06-20 19:16:09 +0530

    react-unity-webgl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import Unity, { UnityContext } from "react-unity-webgl";
 import HeaderNav from './assets/components/header-nav';
 import Photo from './assets/components/photo';
+
+const unityContext = new UnityContext({
+  loaderUrl: "Build1/Build/UnityLoader.js",
+  dataUrl: "Build1/Build/Builds.json",
+  frameworkUrl: "Build1/Build/Builds.wasm.framework.unityweb",
+  codeUrl: "Build1/Build/Builds.wasm.code.unityweb",
+});
 
 function App() {
   return (
@@ -9,6 +17,8 @@ function App() {
         <HeaderNav />
         <Photo />
       </header>
+      <p>Hello</p>
+      <Unity unityContext = {unityContext} />
     </div>
   );
 }
